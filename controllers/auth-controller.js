19adea64b083b9a2c6ef11d41d0d14cd7704db27
Aditya Nehare <adityanehare55@gmail.com>
2025-09-19 17:20:48 +0530

    Fix vercel runtime config

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import Verification from "../models/verification.js";
 import { sendEmail } from "../libs/send-email.js";
