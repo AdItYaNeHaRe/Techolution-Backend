33b244dde75902d2c6d9384c77bdd2746941688c
Aditya Nehare <adityanehare55@gmail.com>
2025-09-19 15:53:17 +0530

    first commit

---
+++ libs/send-email.js
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const fromEmail = process.env.FROM_EMAIL;
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT, 10),
+  secure: process.env.SMTP_SECURE === "true", // true for 465, false for other ports
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export const sendEmail = async (to, subject, html) => {
+  const mailOptions = {
+    from: `Techolution <${fromEmail}>`,
+    to,
+    subject,
+    html,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+    return true;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return false;
+  }
+};
